1e4316d22eae0ab2caf98d9b281ba1cf9507c4a9
haukekoehn <hauke.koehn@uni-potsdam.de>
2026-07-07 10:56:11 +0200

    Update multi_messenger_likelihood to use conditional flows

--- jesterTOV/inference/likelihoods/multimessenger.py
+++ jesterTOV/inference/likelihoods/multimessenger.py
@@ -11,177 +11,217 @@ from jesterTOV.inference.base.likelihood import LikelihoodBase
 from jesterTOV.inference.flows.flow import Flow, ConditionalFlow
 from jesterTOV.logging_config import get_logger
 
+from jesterTOV.utils import solar_mass_in_meter
+
+from .cosmology import convert_to_log10_mej_dyn
+
 logger = get_logger("jester")
 
 
 class MultiMessengerLikelihood(LikelihoodBase):
     """
-    Likelihood for a Gravitational wave event where there is also an independent posterior for the ejecta masses
-    from an electromagnetic counterpart.
+    Multi-messenger likelihood for a single BNS merger.
 
-    The likelihood works by:
-
-    1. Passing population parameters to a population model and generating (m1, m2) pairs.
-    2. Interpolate Λ1, Λ2 from the candidate EOS at
-       the generated mass points, evaluate GW likelihood on (m1, m2, Λ1_EOS, Λ2_EOS),
-       apply penalties for masses exceeding Mtov, and average over all (m1, m2) pairs.
-    3. Determine and log10_mej_dyn and log10_mej_wind from the EOS, m1, m2 using some fitting formulae,
-       and evaluate EM flow log_prob one (log10_mej_dyn, log10_mej_wind).
-    4. Add the log_probs and average over all generated (m1, m2) pairs.
+    This class evaluates the likelihood for a joint inference of the EOS
+    and mass distribution from an EM-bright BNS merger. 
+    The basic mechanism is as follows:
+    
+    1. Sample (m1, m2, d_L, cos_theta_jn) from the GW posterior.
+    2. Interpolate tidal deformabilities (Λ1, Λ2) from the sampled EOS.
+    3. Evaluate the conditional probabilities p(Λ1, Λ2 | m1, m2, d_L, cos_theta_jn, GW)
+       through a conditional normalizing flow.
+    4. Evaluate the mass distribution function on (m1, m2)
+    5. Optional: If asked for, one can additional factor in a light curve posterior
+       by evaluating the flow p(log10_mej_dyn, dL, cos_theta_jn | EM),
+       similar to the MultiMessengerLikelihood. 
+    6. Multiply and sum everything together to get the result.
+    
 
     Parameters
     ----------
     event_name : str
         Name of the GW event (e.g., "GW170817")
-    dir_gw : str
-        Path to directory containing the trained normalizing flow for the GW posterior
-    dir_em : str
-        Path to directory containing the trained normalizing flow for the EM posterior
-    logprior_m1m2: Callable, optional
-        log-prior function that was used in the GW inference for the source frame masses m1, m2.
-        Should be jax-compatible and take m1, m2 as two separate arguments.
-        If None, will simply default to 0 (flat prior).
+    posterior_gw : str
+        Path to an *.npz file containing the GW posterior samples. 
+        Has to include entries for mass_1_source, mass_2_source, luminosity_distance, cos_theta_jn.
+    conditional_flow_gw: str
+        Path to a conditional normalizing flow to evaluate p(Λ1, Λ2 | m1, m2, d_L, cos_theta_jn, GW)
+    mass_model_logpdf: Callable
+        Binary mass function that takes as input jnp Arrays for mass_1, mass_2, and then a dict 
+        with the mass model's parameters. Must be jittable.
+    flow_em: str
+        Path to a normalizing flow to evaluate p(log10_mej_dyn, dL, cos_theta_jn | EM). 
+        If not set, will ignore the EM contribution to the likelihood.
+    use_em: bool, optional
+        Whether to use the EM contribution to the likelihood. If False,
+        flow_em argument will be ignored. (default: True)
+    logprior_gw: Callable | None:
+        Prior used to obtain the GW posterior. 
+        Should be a function that accepts a dict with the GW parameters as keys.
+        If None, uniform priors will be assumed.
+    logprior_em: Callable | None:
+        Prior used to obtain the light curve posterior. 
+        Should be a function that accepts a dict with the EM parameters as keys.
+        If None, uniform priors will be assumed.
     penalty_value : float, optional
         Penalty value for samples where masses exceed Mtov (default: 0.0, i.e. no penalty)
+    N_masses_evaluation : int, optional
+        Number of samples to take from the GW posterior. 
+        Will never be larger than the number of samples in posterior_gw. (default: 2000)
     N_masses_batch_size : int, optional
-        Batch size for jax.lax.map processing (default: 1000)
-    seed : int, optional
-        Random seed for mass pre-sampling (default: 42)
-        Fixed seed ensures reproducibility across runs
-
-    Attributes
-    ----------
-    event_name : str
-        Name of the GW event
-    model_dir : str
-        Path to directory containing the trained normalizing flow model
-    penalty_value : float
-        Penalty value for samples where masses exceed Mtov
-    N_masses_batch_size : int
-        Batch size for processing
-    seed : int
-        Random seed used for pre-sampling
-    flow_gw : Flow
-        Normalizing flow model for the GW event
-    flow_em : Flow
-        Normalizing flow model for the EM event
-
-    Notes
-    -----
-
-    GPU parallelization via jax.lax.map means N=10,000 samples costs nearly
-    the same as N=20, so use large N for near-integration accuracy.
+        Batch size for processing mass samples (default: 1000)
+    key: jax.random.PRNGKey, optional
+        Random key used to initialize the samples. (default: jax.random.key(0))
 
     """
-
-    event_name: str
-    model_dir: str
-    penalty_value: float
-    N_masses_batch_size: int
-    seed: int
-    flow: Flow
-    fixed_mass_samples: Float[Array, "n_samples 2"]
 
     def __init__(
         self,
         event_name: str,
-        dir_gw: str,
-        dir_em: str,
-        logprior_m1m2: Callable | None = None,
-        zeta: float | None = None,
+        posterior_gw: str,
+        conditional_flow_gw: str,
+        mass_model_logpdf: Callable,
+        flow_em: str | None = None,
+        use_em: bool = True,
+        logprior_gw: Callable | None = None,
+        logprior_em: Callable | None = None,
         penalty_value: float = 0.0,
+        N_masses_evaluation: int = 2000,
         N_masses_batch_size: int = 1000,
-        seed: int = 42,
+        key: jax.random.PRNGKey = jax.random.key(0),
     ) -> None:
+        
+
         super().__init__()
         self.event_name = event_name
-        self.dir_gw = dir_gw
-        self.dir_em = dir_em
+
+        self.N_masses_evaluation = N_masses_evaluation
         self.penalty_value = penalty_value
         self.N_masses_batch_size = N_masses_batch_size
-        self.seed = seed
 
-        # setup m1 m2 prior
-        if logprior_m1m2 is None:
-            logprior_m1m2 = lambda m1, m2: 0
-        self.logprior_m1m2 = jax.jit(logprior_m1m2)
+        # set up GW posterior
+        key, subkey = jax.random.split(key)
+        self.setup_gw_likelihood(
+            posterior_gw,
+            conditional_flow_gw,
+            logprior_gw,
+            subkey
+        )
 
-        # Load GW posterior flow for this event
-        logger.info(f"Loading NF for GW posterior {event_name} from {dir_gw}.")
-        self.flow_gw = Flow.from_directory(dir_gw)
+        # set up mass function
+        self.mass_distribution_logpdf = jax.jit(mass_model_logpdf)
 
-        # Setup EM posterior flwo for this event
-        logger.info(f"Loading EM posterior {event_name} from {dir_em}.")
-        if dir_em.endswith("posterior.npz"):
-            # only use log10_mej_dyn in this case
-            posterior = jnp.load(dir_em)
-            hist, bins = jnp.histogram(posterior["log10_mej_dyn"], bins=jnp.linspace(-4, -1.3, 14), density=True)
-            bins = 0.5*(bins[1:] + bins[:-1])
-            log_hist = jnp.log(hist)
-            log_hist = jnp.maximum(log_hist, -200)
+        # set up EM posterior if wanted
+        self.use_em = use_em
+        if self.use_em:
+            self.setup_em_likelihood(
+                flow_em,
+                logprior_em,
+            )
 
-            def em_likelihood(em_sample):
+    def setup_gw_likelihood(
+            self,
+            posterior_gw: str,
+            conditional_flow_gw: str,
+            logprior_gw: Callable | None,
+            key: jax.random.PRNGKey
+        ) -> None:
 
-                log10_mej_dyn = jnp.maximum(em_sample[0], bins[0])
+        # set up integration arrays
+        posterior_gw = jnp.load(posterior_gw)
+        Nsamp = posterior_gw["mass_1_source"].shape[0]
 
-                dyn_ejecta_prob = jnp.interp(log10_mej_dyn, bins, log_hist, left=-200, right=-200)
+        if Nsamp < self.N_masses_evaluation:
+            logger.warning(f"For event {self.event_name}, N_masses_evaluation is larger than available GW posterior samples.",
+                           "Setting N_masses_evaluation from {self.N_masses_evaluation} to {Nsamp}.")
+            self.N_masses_evaluation = Nsamp
 
-                return dyn_ejecta_prob
+        mask = jnp.zeros(Nsamp, dtype=bool).at[
+            jax.random.choice(key, Nsamp, (self.N_masses_evaluation,), replace=False)
+        ].set(True)
+
+        self.mass_1_source = posterior_gw["mass_1_source"][mask]
+        self.mass_2_source= posterior_gw["mass_2_source"][mask]
+        self.luminosity_distance = posterior_gw["luminosity_distance"][mask]
+        self.cos_theta_jn = posterior_gw["cos_theta_jn"][mask]
+
+        # Load GW conditional flow for this event 
+        self.cflow_gw = ConditionalFlow.from_directory(conditional_flow_gw)
+
+
+        # Set up prior subtraction 
+        if logprior_gw is None:
+            logprior_gw = lambda x: 0
+        self.logprior_gw = jax.jit(logprior_gw)
+
+        logger.info(f"Loaded GW likelihood for {self.event_name} from {posterior_gw} and {conditional_flow_gw}.")
+
+    def setup_em_likelihood(
+            self,
+            flow_em: str,
+            logprior_em: Callable | None,
+    ) -> None:
         
-        elif dir_em.endswith("nf"):
-            # this is interpreted that there is a conditional flow on the wind ejecta
-            posterior_file = "/".join(dir_em.split("/")[:-1]) + "/posterior.npz"
-            posterior = jnp.load(posterior_file)
-            hist, bins = jnp.histogram(posterior["log10_mej_dyn"], bins=jnp.linspace(-4, -1.3, 14), density=True)
-            bins = 0.5*(bins[1:] + bins[:-1])
-            log_hist = jnp.log(hist)
-            log_hist = jnp.maximum(log_hist, -200)
+        # Load EM flow for this event
+        self.flow_em = Flow.from_directory(flow_em)
 
-            self.cflow_wind = ConditionalFlow.from_directory(dir_em)
+        # Set up prior subtraction
+        if logprior_em is None:
+            logprior_em = lambda x: 0
+        self.logprior_em = jax.jit(logprior_em)
 
-            if zeta is None:
-                u = jnp.linspace(0, 1, 10)
+        logger.info(f"Loaded EM likelihood for {self.event_name} from {flow_em}.")
 
-                def em_likelihood(em_sample):
-
-                    log10_mej_dyn = jnp.maximum(em_sample[0], bins[0])
-                    log10_mdisk = jnp.maximum(em_sample[1], -3.9)
-
-                    dyn_ejecta_prob = jnp.interp(log10_mej_dyn, bins, log_hist, left=-200, right=-200)
-
-                    log10_mej_wind_arr = -4 + u * (log10_mdisk + 4)
-
-                    log_pdf = self.cflow_wind.log_prob(
-                        x=log10_mej_wind_arr.reshape(-1, 1), 
-                        y=jnp.full((u.shape[0],1), em_sample[0])
-                    )
-
-                    wind_ejecta_prob = jnp.log(
-                        jnp.mean(jnp.exp(log_pdf))
-                    )
-
-                    return dyn_ejecta_prob + wind_ejecta_prob
-            else:
-                self.log10_zeta = jnp.log10(zeta)
-
-                def em_likelihood(em_sample):
-                    
-                    log10_mej_dyn, log10_mdisk = em_sample[0], em_sample[1]
-
-                    dyn_ejecta_prob = jnp.interp(log10_mej_dyn, bins, log_hist, left=-200, right=-200)
-
-                    log10_mej_wind = self.log10_zeta + log10_mdisk
-                    log10_mej_wind = jnp.maximum(-3.9, log10_mej_wind)
-
-                    wind_ejecta_prob = self.cflow_wind.log_prob(x=log10_mej_wind, y=log10_mej_dyn)
-
-                    return dyn_ejecta_prob + wind_ejecta_prob
-
+    def process_gw_likelihood(
+            self,
+            samples: dict[str, Array]
+    ) -> Array:
         
-        self.flow_em = jax.jit(em_likelihood)
+        def process_single(sample):
+            
+            # Evaluate GW log_posterior on single sample
+            cond_sample = jnp.array([sample["mass_1_source"], sample["mass_2_source"], sample["luminosity_distance"], sample["cos_theta_jn"]])
+            eval_sample = jnp.array([sample["lambda_1"], sample["lambda_2"]])
+            logpdf_gw = self.cflow_gw.log_prob(eval_sample, cond_sample)
 
-        logger.info(f"Loaded NFs for {event_name}.")
+            # subtract the prior
+            logprior_gwvalue = self.logprior_gw(sample)
+            logpdf_gw -= logprior_gwvalue
 
+            return logpdf_gw
+        
+        log_probs_gw = jax.lax.map(
+            process_single,
+            samples,
+            batch_size=self.N_masses_batch_size
+        )
+
+        return log_probs_gw
+    
+    def process_em_likelihood(
+            self,
+            samples: dict[str, Array]
+        ) -> Array:
+
+
+        def process_single(sample):
+            # Evaluate log
+            array_sample = jnp.array([sample["log10_mej_dyn"], sample["luminosity_distance"], sample["cos_theta_jn"]])
+            logpdf_em = self.flow_em.log_prob(array_sample)
+
+            #subtract the prior
+            logprior_emvalue = self.logprior_em(sample)
+            logpdf_em -= logprior_emvalue
+
+            return logpdf_em
+        
+        log_probs_em = jax.lax.map(
+            process_single,
+            samples,
+            batch_size=self.N_masses_batch_size
+        )
+
+        return log_probs_em
 
     def evaluate(self, params: dict[str, Float | Array]) -> Float:
         """
@@ -194,80 +234,60 @@ class MultiMessengerLikelihood(LikelihoodBase):
             - 'masses_EOS': Array of neutron star masses from EOS
             - 'radii_EOS': Array of neutron star radii from EOS
             - 'Lambdas_EOS': Array of tidal deformabilities from EOS
-            - 'masses_1_pop': Array of heavier NS masses sampled from the population
-            - 'masses_2_pop': Array of ligher NS sampled from the population
-            - 'log10_mej_dyn': Array of dynamical ejecta from the NS masses
-            - 'log10_mdisk': Array of disk masses from the NS masses
+            - population parameters for self.mass_distribution_logpdf
         Returns
         -------
         Float
-            Log likelihood value for this GW event
+            Log likelihood value for this multi-messenger event
         """
-        # Extract EOS parameters (no _random_key needed!)
-        masses_EOS: Float[Array, " n_points"] = params["masses_EOS"]
-        Lambdas_EOS: Float[Array, " n_points"] = params["Lambdas_EOS"]
-        mtov: Float = jnp.max(masses_EOS)
 
-        mm_samples = jnp.array([params["masses_1_pop"],
-                                params["masses_2_pop"],
-                                params["log10_mej_dyn"],
-                                params["log10_mdisk"]]).T
+        mtov = params["masses_EOS"].max()
+
+        mass_distribution_logprobs = self.mass_distribution_logpdf(
+            self.mass_1_source, 
+            self.mass_2_source, 
+            params
+        )
         
+        # get tidal deformabilities
+        lambda_1 = jnp.interp(self.mass_1_source, params["masses_EOS"], params["Lambdas_EOS"])
+        lambda_2 = jnp.interp(self.mass_2_source, params["masses_EOS"], params["Lambdas_EOS"])
 
-
-        def process_sample(sample: Float[Array, " 4"]) -> Float:
-            """
-            Process a single pre-sampled mass pair
-
-            Note: jax.lax.map with batch_size applies function to individual
-            elements. The batch_size parameter is for compilation optimization.
-
-            Parameters
-            ----------
-            sample : Float[Array, " 4"]
-                Pre-sampled mass pair [m1, m2]
-
-            Returns
-            -------
-            Float
-                Log probability including penalties for this sample
-            """
-            m1 = sample[0]
-            m2 = sample[1]
-            log10_mej_dyn = sample[2]
-            log10_mdisk = sample[3]
-
-
-            # Interpolate lambdas from candidate EOS
-            lambda_1 = jnp.interp(m1, masses_EOS, Lambdas_EOS, right=1.0)
-            lambda_2 = jnp.interp(m2, masses_EOS, Lambdas_EOS, right=1.0)
-
-            # Evaluate GW log_posterior on single sample
-            gw_sample = jnp.array([m1, m2, lambda_1, lambda_2])
-            logpdf_gw = self.flow_gw.log_prob(gw_sample)
-
-            # subtract the prior
-            logprior = self.logprior_m1m2(m1, m2)
-            logpdf_gw -= logprior
-
-            # Penalties for masses exceeding Mtov
-            penalty_m1 = jnp.where(m1 > mtov, self.penalty_value, 0.0)
-            penalty_m2 = jnp.where(m2 > mtov, self.penalty_value, 0.0)
-            
-            # Evaluate log
-            em_sample = jnp.array([log10_mej_dyn, log10_mdisk])
-            logpdf_em = self.flow_em(em_sample)
-
-            # Return log prob + penalties for this sample
-            return logpdf_gw + penalty_m1 + penalty_m2 + logpdf_em
-        
-        # Use jax.lax.map with batching for memory-efficient processing
-        # Process all pre-sampled mass pairs
-        all_logprobs = jax.lax.map(
-            process_sample, mm_samples, batch_size=self.N_masses_batch_size
+        # collect all in one dict
+        samples = dict(
+            mass_1_source=self.mass_1_source,
+            mass_2_source=self.mass_2_source,
+            lambda_1=lambda_1,
+            lambda_2=lambda_2,
+            luminosity_distance=self.luminosity_distance,
+            cos_theta_jn=self.cos_theta_jn
         )
 
-        # Take logsumexp over all pre-sampled mass pairs
-        log_likelihood = logsumexp(all_logprobs) - jnp.log(mm_samples.shape[0])
+        # evaluate GW log probs
+        logprobs_gw = self.process_gw_likelihood(samples)
+
+        log_probs = mass_distribution_logprobs + logprobs_gw
+
+        # evaluate EM log probs if wanted
+        if self.use_em:
+            m_coll = params["k_coll"] * mtov
+            radii_1 = jnp.interp(self.mass_1_source, params["masses_EOS"], params["radii_EOS"])
+            radii_2 = jnp.interp(self.mass_2_source, params["masses_EOS"], params["radii_EOS"])
+            compactness_1 = self.mass_1_source / radii_1 * solar_mass_in_meter * 1e-3
+            compactness_2 = self.mass_2_source / radii_2 * solar_mass_in_meter * 1e-3
+            log10_mej_dyn = convert_to_log10_mej_dyn(
+                m_coll,
+                self.mass_1_source, compactness_1, lambda_1,
+                self.mass_2_source, compactness_2, lambda_2
+                )
+            samples["log10_mej_dyn"] = log10_mej_dyn
+
+            logprobs_em = self.process_em_likelihood(samples)
+            log_probs += logprobs_em
+
+    
+        # Take logsumexp over all samples
+        log_likelihood = logsumexp(log_probs) - jnp.log(log_probs.shape[0])
+       
 
         return log_likelihood
